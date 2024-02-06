5e9493f52ed46c91bc69e72c0c43707920f071d6
Ives Chiba <ivesjundi@gmail.com>
2024-02-06 14:02:20 -0300

    Creating project and Git repository

---
+++ src/DbException.java
@@ -0,0 +1,6 @@
+public class DbException extends RuntimeException{
+
+    public DbException(String message) {
+        super(message);
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
