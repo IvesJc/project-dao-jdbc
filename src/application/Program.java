384b5d2901ebb8802b5eabe3c9584201221385a8
Ives Chiba <ivesjundi@gmail.com>
2024-02-07 12:26:26 -0300

    Creating DepartmentDao and SellerDao Interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,7 @@
 package application;
 
-import model.db.DB;
 import model.entities.Department;
-import model.entities.Seller;
 
-import java.sql.Connection;
 import java.util.Date;
 
 public class Program {
